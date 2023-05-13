71a5dd615a4364611c3440ffc9227c72e5797f12
Ilias Dzhumabekov <ilias.dzhumabekov@gmail.com>
2023-05-13 23:11:59 +0200

    add login pass

--- src/main/java/com/fitness/ptp/controller/LoginController.java
+++ src/main/java/com/fitness/ptp/controller/LoginController.java
@@ -1,15 +1,19 @@
-package com.spring.controller;
+package com.fitness.ptp.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LoginController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage() {
-        return "login";
+    @GetMapping(value = "/login")
+    public String login() {
+        return "Enter login and password";
     }
 
 }
